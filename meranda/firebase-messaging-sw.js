cfe06849b25efbf05d9b3d3a73c153b9d56ce547
SonnyJones <juliuslamarjones@gmail.com>
2026-08-23 06:51:17 -0500

    Add Firebase messaging service worker for background lock-screen pushes

---
+++ meranda/firebase-messaging-sw.js
@@ -0,0 +1,23 @@
+﻿importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+    apiKey: "AIzaSyDxFse5ziXWYyzOimzdWjqNNDgsvzD-rvo",
+    authDomain: "meranda-grooming.firebaseapp.com",
+    projectId: "meranda-grooming",
+    storageBucket: "meranda-grooming.firebasestorage.app",
+    messagingSenderId: "583723658573",
+    appId: "1:583723658573:web:e9e074ffacbf29f1ad5b04"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+        icon: 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=https://juliuslamarjones.github.io/meranda/'
+    };
+    self.registration.showNotification(notificationTitle, notificationOptions);
+});
